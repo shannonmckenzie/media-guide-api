1c427f7ce726e2be3fce59cb945ff4dccf89d81f
Taylor Culbreth <taylorculbreth@taylorculbreth.com>
2015-04-28 21:37:47 -0500

    Created sharedChannelGroupFactory and DTO.SharedChannelGroup enttity

---
+++ MediaGuide.DTO/SharedChannelGroup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaGuide.DTO
+{
+    public class SharedChannelGroup
+    {
+        public Int32 Id { get; set; }
+        public String UserId { get; set; }
+        public String ChannelGroupId { get; set; }
+    }
+}
---
+++ MediaGuide.Repository/Factories/SharedChannelGroupFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediaGuide.Repository.Entities;
+
+namespace MediaGuide.Repository.Factories
+{
+    public class SharedChannelGroupFactory
+    {
+        public DTO.SharedChannelGroup CreateSharedChannelGroup(SharedChannelGroup sharedChannelGroup)
+        {
+            return new DTO.SharedChannelGroup()
+            {
+                Id = sharedChannelGroup.Id,
+                UserId = sharedChannelGroup.UserId,
+                ChannelGroupId = sharedChannelGroup.ChannelGroupId
+            };
+        }
+
+        public SharedChannelGroup CreateSharedChannelGroup(DTO.SharedChannelGroup sharedChannelGroup)
+        {
+            return new SharedChannelGroup()
+            {
+                Id = sharedChannelGroup.Id,
+                UserId = sharedChannelGroup.UserId,
+                ChannelGroupId = sharedChannelGroup.ChannelGroupId
+            };
+        }
+    }
+}
