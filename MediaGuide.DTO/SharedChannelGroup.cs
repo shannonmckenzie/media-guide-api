ddd47e4cf012d770724059fe78f77f6a0e2c52e1
taylorculbreth <tsc@taylorculbreth.com>
2015-07-07 22:40:44 -0500

    Created SharedChannelGroup DTO and created SharedChannelGroupFactory

---
+++ MediaGuide.DTO/SharedChannelGroup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaGuide.DTO
+{
+    public class SharedChannelGroup
+    {
+        public Int32 Id { get; set; }
+        public String UserId { get; set; }
+        public String ChannelGroupId { get; set; }
+    }
+}
