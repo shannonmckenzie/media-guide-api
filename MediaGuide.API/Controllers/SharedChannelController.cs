fa70fa8edbfecd72b678ee56a627c8210e070852
taylorculbreth <tsc@taylorculbreth.com>
2015-11-08 23:05:36 -0600

    Finished off sharedchannelcontroller.

--- MediaGuide.API/Controllers/SharedChannelController.cs
+++ MediaGuide.API/Controllers/SharedChannelController.cs
@@ -68,15 +68,85 @@ namespace MediaGuide.API.Controllers
         }
 
         public IHttpActionResult put([FromBody] DTO.SharedChannel sharedChannel){
+            try
+            {
+                if (sharedChannel == null)
+                {
+                    return BadRequest();
+                }
 
+                var shdCh = _sharedChannelFactory.CreateSharedChannel(sharedChannel);
+                var result = _repository.UpdateSharedChannel(shdCh);
+
+                if (result.Status == RepositoryActionStatus.Updated)
+                {
+                    var updatedSharedChannel = _sharedChannelFactory.CreateSharedChannel(result.Entity);
+                    return Ok(updatedSharedChannel);
+                }
+                else if (result.Status == RepositoryActionStatus.NotFound)
+                {
+                    return NotFound();
+                }
+                return BadRequest();
+            }
+            catch (Exception)
+            {
+                return InternalServerError();
+            }
         }
 
-        public IHttpActionResult patch(int id,[FromBody] JsonPatchDocument<DTO.SharedChannel> sharedChannel){
+        public IHttpActionResult patch(int id,[FromBody] JsonPatchDocument<DTO.SharedChannel> sharedChannelPatchDocument){
+            try
+            {
+                if (sharedChannelPatchDocument == null)
+                {
+                    return BadRequest();
+                }
 
+                var sharedChannel = _repository.GetSharedChannel(id);
+                if (sharedChannel == null)
+                {
+                    return NotFound();
+                }
+
+                var shdCh = _sharedChannelFactory.CreateSharedChannel(sharedChannel);
+                sharedChannelPatchDocument.ApplyTo(shdCh);
+
+                var result = _repository.UpdateSharedChannel(_sharedChannelFactory.CreateSharedChannel(shdCh));
+
+                if (result.Status == RepositoryActionStatus.Updated)
+                {
+                    var patchedSharedChannel = _sharedChannelFactory.CreateSharedChannel(result.Entity);
+                    return Ok(patchedChannel);
+                }
+                return BadRequest();
+            }
+            catch (Exception)
+            {
+                return InternalServerError();
+            }
         }
 
         public IHttpActionResult delete(int id){
+            try
+            {
+                var result = _repository.DeleteSharedChannel(id);
 
+                if (result.Status == RepositoryActionStatus.Deleted)
+                {
+                    return StatusCode(HttpStatusCode.NoContent);
+                }
+                else if (result.Status == RepositoryActionStatus.NotFound)
+                {
+                    return NotFound();
+                }
+                return BadRequest();
+            }
+            catch (Exception)
+            {
+                
+                return InternalServerError;
+            }
         }
     }
 }
