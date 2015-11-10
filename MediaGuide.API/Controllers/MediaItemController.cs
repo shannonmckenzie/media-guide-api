26efcd507a95811f127eb6ca7598aed44a929d9a
taylorculbreth <tsc@taylorculbreth.com>
2015-11-09 21:34:32 -0600

    Finished up mediaitemcontroller

--- MediaGuide.API/Controllers/MediaItemController.cs
+++ MediaGuide.API/Controllers/MediaItemController.cs
@@ -24,23 +24,136 @@ namespace MediaGuide.API.Controllers
         }
 
         public IHttpActionResult Get(int id){
-
+            try 
+	        {	        
+	            var mediaItem = _repository.GetMediaItem(id);
+	            if(mediaItem == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return Ok(_mediaItemFactory.CreateMediaItem(mediaItem));
+                }
+	        }
+	        catch (Exception)
+	        {
+	        	return InternalServerError();
+	        }
         }
 
         public IHttpActionResult post([FromBody] DTO.MediaItem mediaItem){
+            try 
+	        {	        
+	        	if(mediaItem == null)
+                {
+                    return BadRequest();
+                }
 
+                var mdItm = _mediaItemFactory.CreateMediaItem(mediaItem);
+                var result = _repository.InsertMediaItem(mdItem);
+
+                if(result.Status == RepositoryActionStatus.Created)
+                {
+                    var newMediaItem = _mediaItemFactory.CreateMediaItem(result.Entity);
+                    return Created(Request.RequestUri + "/" + newMediaItem.Id.ToString(), newMediaItem);
+                }
+
+                return BadRequest();
+	        }
+	        catch (Exception)
+	        {
+	        	return InternalServerError();
+	        }
         }
 
-        public IHttpActionResult put([FromBody] DTO.MediaItem mediaItem){
+        public IHttpActionResult put([FromBody] DTO.MediaItem mediaItem)
+        {
+            try
+            {
+                if(mediaItem == null)
+                {
+                    return BadRequest();
+                }
 
+                var mdItm = _mediaItemFactory.CreateMediaItem(mediaItem);
+                var result = _repository.UpdateMediaItem(mdItm);
+
+                if(result.Status = RepositoryActionStatus.Updated)
+                {
+                    var updatedMediaItem = _mediaItemFactory.CreateMediaItem(result.Entity);
+
+                    return Ok(updatedMediaItem);
+                }
+                else if(result.Status = RepositoryActionStatus.NotFound)
+                {
+                    return NotFound();
+                }
+                return BadRequest();
+            }
+            catch(Exception)
+            {
+                return InternalServerError();
+            }
         }
 
-        public IHttpActionResult patch(int id,[FromBody] JsonPatchDocument<DTO.MediaItem> mediaItem){
+        public IHttpActionResult patch(int id,[FromBody] JsonPatchDocument<DTO.MediaItem> mediaItemPatchDocument)
+        {
+            try
+            {
+                if (mediaItemPatchDocument == null)
+                {
+                    return BadRequest();
+                }
 
+                var mediaItem = _repository.GetMediaItem(id);
+                if (mediaItem == null)
+                {
+                    return NotFound();
+                }
+
+                // map
+                var mdItem = _mediaItemFactory.CreateMediaItem(mediaItem);
+
+                mediaItemPatchDocument.ApplyTo(mdItem);
+
+                var result = _repository.UpdateMediaItem(_mediaItemFactory.CreateMediaItem(mdItm));
+
+                if (result.Status == RepositoryActionStatus.Updated)
+                {
+                    // map to dto
+                    var patchedMediaItem = _mediaItemFactory.CreateMediaItem(result.Entity);
+                    return Ok(patchedMediaItem);
+                }
+
+                return BadRequest();
+            }
+            catch (Exception)
+            {
+                return InternalServerError();
+            }
         }
 
-        public IHttpActionResult delete(int id){
+        public IHttpActionResult delete(int id)
+        {
+            try
+            {
+                var result = _repository.DeleteMediaItem(id);
 
+                if(result.Status == RepositoryActionStatus.Deleted)
+                {
+                    return StatusCode(HttpStatusCode.NoContent);
+                }
+                else if(result.Status = RepositoryActionStatus.NotFound)
+                {
+                    return NotFound();
+                }
+                return BadRequest();
+            }
+            catch(Exception)
+            {
+                return InternalServerError();
+            }
         }
     }
 }
